976477d0cf8902e82b350570435a8590cd269092
oziroe <oziroe@gmail.com>
2017-08-01 05:05:26 +0800

    Rewrite script, successfully!

--- try2048.js
+++ try2048.js
@@ -15,21 +15,21 @@
 function Advent(x, y, number, finished)
 {
     console.log("Number " + number + " appears at " + StringP(x, y));
-    finished();
+    DisplayAdvent(x, y, number, finished);
 }
 
 function Move(x, y, newX, newY, finished)
 {
     console.log("The number at " + StringP(x, y) + " is now at " +
         StringP(newX, newY));
-    finished();
+    DisplayMove(x, y, newX, newY, finished);
 }
 
 function Substitute(x, y, number, finished)
 {
-    console.log("The number at" + StringP(x, y) + " is now changed to " +
+    console.log("The number at " + StringP(x, y) + " is now changed to " +
         number);
-    finished();
+    DisplaySubstitute(x, y, number, finished);
 }
 
 
@@ -61,15 +61,21 @@ function Turn()
     {
         self.Move(x, y, toX, toY);
         self._remain.substitute++;
-        self._actions.substitute.push({x: x, y: y, number: number * 2});
+        self._actions.substitute.push({x: toX, y: toY, number: number * 2});
     };
 
-    this.Trigger = function()
+    this.Trigger = function(afterMove=null, afterAdvent=null)
     {
-        function CommonPatternHere(dataSet, count, call, after)
+        function CommonPatternHere(dataSet, call, decrease, beforeNext,
+            after)
         {
             return function()
             {
+                if (dataSet.length === 0)
+                {
+                    after();
+                    return;
+                }
                 while (dataSet.length > 0)
                 {
                     var data = dataSet.pop();
@@ -77,11 +83,14 @@ function Turn()
                     {
                         // It is heard that in Javascript there's no data race.
                         // The following code need fix if I was wrong.
-                        // It is neccessary to wrap `count` into an object so
-                        // I can change its value here.
-                        count.value--;
-                        if (count.value === 0)
+                        // `decrease` will change count value and return its new
+                        // value. It must be wrapped to change the origin var.
+                        if (decrease() === 0)
+                        {
+                            if (beforeNext !== null)
+                                beforeNext();
                             after();
+                        }
                     });
                 }
             };
@@ -94,9 +103,11 @@ function Turn()
         {
             this._list = [];
             var self = this;
-            this.Call = function(dataSet, count, call)
+            this.Call = function(dataSet, call, decrease,
+                beforeNext=null)
             {
-                self._list.push({dataSet: dataSet, count: count, call: call});
+                self._list.push({dataSet: dataSet, call: call,
+                    decrease: decrease, beforeNext: beforeNext});
                 return self;
             };
             this.List = function()
@@ -106,27 +117,31 @@ function Turn()
                     return function(after)
                     {
                         return CommonPatternHere(argument.dataSet,
-                            argument.count, argument.call, after);
+                            argument.call, argument.decrease,
+                            argument.beforeNext, after);
                     };
                 });
             };
         }
         new CurryList()
-        .Call(self._actions.move, {value: self._remain.move},
-            function(move, finished)
-            {
-                Move(move.x1, move.x2, move.y1, move.y2, finished);
-            })
-        .Call(self._actions.substitute, {value: self._remain.substitute},
-            function(sub, finished)
-            {
+        .Call(self._actions.move,
+            function(move, finished) {
+                Move(move.x1, move.y1, move.x2, move.y2, finished);
+            }, function() {
+                return --self._remain.move;
+            }, afterMove)
+        .Call(self._actions.substitute,
+            function(sub, finished) {
                 Substitute(sub.x, sub.y, sub.number, finished);
+            }, function() {
+                return --self._remain.substitute;
             })
-        .Call(self._actions.advent, {value: self._remain.advent},
-            function(advent, finished)
-            {
+        .Call(self._actions.advent,
+            function(advent, finished) {
                 Advent(advent.x, advent.y, advent.number, finished);
-            })
+            }, function() {
+                return --self._remain.advent;
+            }, afterAdvent)
         .List().reduceRight(
             function(after, previous) { return previous(after); },
             function() { console.log("All done for this turn."); })();
@@ -144,7 +159,7 @@ function Grid(size)
     for (var i = 0; i < this._grid.length; i++)
         this._grid[i] = new Array(size).fill(0);  // 0 means no number here.
     var self = this;
-    
+
     this.AddRandom = function(turn)
     {
         var full = true;
@@ -175,7 +190,205 @@ function Grid(size)
         }
     };
 
+    // `direction`: 0 => up, 1 => right, 2 => down, 3 => left.
+    this.Slide = function(direction, turn)
+    {
+        // Slide `numbers` toward numbers[0].
+        function SlideVector(numbers, move, merge)
+        {
+            var merged = new Array(size).fill(false);
+            var changed = false;
+            for (var i = 1; i < size; i++)  // First number stays.
+            {
+                if (numbers[i] === 0)
+                    continue;
 
+                for (var j = i - 1; j >= 0; j--)
+                {
+                    if (merged[j])  // Merged number will not be modified again.
+                    {
+                        if (j + 1 !== i)
+                        {
+                            changed = true;
+                            move(i, j + 1);
+                            numbers[j + 1] = numbers[i];
+                            numbers[i] = 0;
+                        }
+                        break;
+                    }
+
+                    if (numbers[j] !== 0)
+                    {
+                        if (numbers[j] === numbers[i])  // Merge same numbers.
+                        {
+                            changed = true;
+                            merge(i, j, numbers[i]);
+                            merged[j] = true;
+                            numbers[j] += numbers[i];
+                            numbers[i] = 0;
+                        }
+                        else  // Move to neighbour.
+                        {
+                            if (j + 1 !== i)
+                            {
+                                changed = true;
+                                move(i, j + 1);
+                                numbers[j + 1] = numbers[i];
+                                numbers[i] = 0;
+                            }
+                        }
+                        break;
+                    }
+
+                    if (j === 0)  // Stop moving on the edge.
+                    {
+                        changed = true;
+                        move(i, 0);
+                        numbers[0] = numbers[i];
+                        numbers[i] = 0;
+                        break;
+                    }
+                }
+            }
+            return changed;
+        }
+
+        var UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3;
+        var changed = false;
+        for (var first = 0; first < size; first++)
+        {
+            var numbers = new Array(size), iX = new Array(size),
+                iY = new Array(size);
+            for (var second = 0; second < size; second++)
+            {
+                var sec = direction === UP || direction === LEFT ? second :
+                    size - second - 1;
+                // I hate 80 ruler.
+                iX[second] =
+                    direction === UP || direction === DOWN ? first : sec;
+                iY[second] =
+                    direction === UP || direction === DOWN ? sec : first;
+                // console.log(second + " " + StringP(iX[second], iY[second]));
+                numbers[second] = self._grid[iX[second]][iY[second]];
+            }
+            // It's a little breaking my rule to put left brace on the same line
+            // of previous code, but it seems too ugly otherwise.
+            var c = SlideVector(numbers, function(from, to) {
+                turn.Move(iX[from], iY[from], iX[to], iY[to]);
+                self._grid[iX[to]][iY[to]] = self._grid[iX[from]][iY[from]];
+                self._grid[iX[from]][iY[from]] = 0;
+            }, function(from, to, number) {
+                turn.Merge(iX[from], iY[from], iX[to], iY[to], number);
+                self._grid[iX[to]][iY[to]] += self._grid[iX[from]][iY[from]];
+                self._grid[iX[from]][iY[from]] = 0;
+            });
+            changed = c ? true : changed;
+        }
+
+        if (changed)
+            self.AddRandom(turn);
+        return changed;
+    };
+
+    this.Over = function()
+    {
+        for (var x = 0; x < size; x++)
+        {
+            for (var y = 0; y < size; y++)
+            {
+                if (self._grid[x][y] !== 0)
+                    return false;
+                if ((x > 0 && self._grid[x - 1][y] === self._grid[x][y]) ||
+                    (x < size - 1 && self._grid[x + 1][y]
+                        // I hate 80 ruler, again.
+                        === self._grid[x][y]) ||
+                    (y > 0 && self._grid[x][y - 1] === self._grid[x][y]) ||
+                    (y < size - 1 && self._grid[x][y + 1]
+                        === self._grid[x][y]))
+                    return false;
+            }
+        }
+        return true;
+    };
+}
+
+
+// Display Part.
+// This is the base of a more complicated implementation of interfaces.
+function DisplayInitialize()
+{
+    var container = document.createElement("div");
+    container.id = "board-tiles-container";
+    container.style.width = container.style.height = "450px";
+    document.getElementById("board-container").appendChild(container);
+}
+
+var searchTable = new Object, registryTable = new Object, deadQueue = new Array;
+
+function DisplayAdvent(x, y, number, finished)
+{
+    var tile = document.createElement("div");
+    tile.innerText = number;
+    tile.classList.add("board-tile");
+    tile.style.width = tile.style.height = tile.style.lineHeight = "100px";
+    tile.style.top  = (y * 110 + 10) + "px";
+    tile.style.left = (x * 110 + 10) + "px";
+    tile.style.transition = "top 0.2s, left 0.2s";
+    document.getElementById("board-tiles-container").appendChild(tile);
+    searchTable[StringP(x, y)] = tile;
+    finished();
+}
+
+function DisplayMove(x, y, newX, newY, finished)
+{
+    var tile = searchTable[StringP(x, y)];
+    tile.style.top  = (newY * 110 + 10) + "px";
+    tile.style.left = (newX * 110 + 10) + "px";
+    // There's no need to worry about overriding.
+    // Any override tile will be override again by substitution.
+    // So any of them survives will be okay.
+    if (registryTable[StringP(newX, newY)] !== undefined)
+    {
+        var dead = registryTable[StringP(newX, newY)];
+        deadQueue.push(dead);
+    }
+
+    delete searchTable[StringP(x, y)];
+    registryTable[StringP(newX, newY)] = tile;
+    tile.addEventListener("transitionend", function()
+    {
+        // console.log("On transition end.");
+        tile.removeEventListener("transitionend", arguments.callee);
+        finished();
+    });
+}
+
+function DisplayAfterMove()
+{
+    while (deadQueue.length > 0)
+    {
+        var dead = deadQueue.pop();
+        dead.parentElement.removeChild(dead);
+    }
+
+    Object.keys(registryTable).forEach(function(pos)
+    {
+        if (searchTable[pos] !== undefined)
+        {
+            var dead = searchTable[pos];
+            dead.parentElement.removeChild(dead);
+        }
+        searchTable[pos] = registryTable[pos];
+    });
+    registryTable = new Object;
+    // console.log(searchTable);
+}
+
+function DisplaySubstitute(x, y, num, finished)
+{
+    var tile = searchTable[StringP(x, y)];
+    tile.innerText = num;
+    finished();
 }
 
 
@@ -183,11 +396,39 @@ function Grid(size)
 // The main game loop and entry point.
 window.onload = function()
 {
+    DisplayInitialize();
     var turn = new Turn();
-    turn.Advent(3, 2, 16);
-    turn.Merge(2, 1, 0, 1, 2);
-    turn.Move(3, 3, 0, 3);
+    var grid = new Grid(4);
+    grid.AddRandom(turn);
+    grid.AddRandom(turn);
     turn.Trigger();
+    // console.log(grid);
+
+    var animating = false, over = false;
+    window.addEventListener("keypress", function(e)
+    {
+        // Disable key press during animation.
+        if (animating || over)
+            return;
+        var map = {w: 0, d: 1, s: 2, a: 3};
+        if (e.key in map)
+        {
+            if (grid.Slide(map[e.key], turn))
+            {
+                animating = true;
+                turn.Trigger(DisplayAfterMove, function() {
+                    animating = false;
+                    console.log(StringG(grid));
+                    if (grid.Over())
+                    {
+                        console.log("Game Over");
+                        over = true;
+                    }
+                });
+            }
+            // console.log(searchTable);
+        }
+    });
 }
 
 
@@ -201,4 +442,19 @@ function StringP(x, y)
 function Random(range)
 {
     return Math.floor(Math.random() * range);
+}
+
+function StringG(grid)
+{
+    var str = "";
+    // Only work on square.
+    for (var y = 0; y < grid._grid[0].length; y++)
+    {
+        for (var x = 0; x < grid._grid.length; x++)
+        {
+            str += grid._grid[x][y] + " ";
+        }
+        str += "\n";
+    }
+    return str;
 }
